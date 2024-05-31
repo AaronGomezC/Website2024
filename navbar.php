a2e55d431624d77f2ffbb7c4c75b360b56ad33c7
AaronGomezC <168867785+AaronGomezC@users.noreply.github.com>
2024-05-31 13:10:28 -0600

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,17 +1,15 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">SITIO 2024</a>
-        
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-    
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item"><a class="nav-link active" aria-current="page" href="Index.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="login.php">Log In</a></li>
-        <li class="nav-item"></li><a class="nav-link" href="signup.php">Register</a></li>
+<nav class="navbar navbar-expand-lg bg-primary pt-3">
+  <div class="container-fluid">
+
+    <h1><a class="navbar-brand text-white" href="index.php">Gorras y Sombreros</a></h1>
     </ul>
-</div>
-</div>
+    <form class="d-flex">
+      <a class="btn btn-outline-light mx-2" type="submit" href="table.php">Table</a>
+      <a class="btn btn-outline-light mx-2" type="submit" href="catalogo.php">Catalogo</a>
+      <a class="btn btn-outline-light mx-2" type="submit" href="signup.php">Signup</a>
+      <a class="btn btn-outline-light mx-2" type="submit" href="login.php">Login</a>
+      <a class="btn btn-outline-light mx-2" type="submit" href="logout.php">Logout</a>
+    </form>
+
+  </div>
 </nav>
