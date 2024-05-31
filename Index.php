bc15bbcd521fc90e17c150f7f64ecd311a80b80c
AaronGomezC <168867785+AaronGomezC@users.noreply.github.com>
2024-05-31 01:55:31 -0600

    Update Index.php

--- Index.php
+++ Index.php
@@ -6,17 +6,6 @@
     <title>WEBSITE DEMO</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  
-
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>WEBSITE MAY 2024</title>
-        <meta name="description" content="">
-        <link rel="shortcut icon" href="favicon.ico">
-        <link rel="stylesheet" href="css/main.css">
-    </head>
 
     <body>
         <h1>TITLE</h1>
