48e8d4459e6824c04925eca9996d591048f42069
AaronGomezC <168867785+AaronGomezC@users.noreply.github.com>
2024-05-31 07:13:33 -0600

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">SITIO 2024</a>
+        
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link active" aria-current="page" href="Index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="login.php">Log In</a></li>
+        <li class="nav-item"></li><a class="nav-link" href="signup.php">Register</a></li>
+    </ul>
+</div>
+</div>
+</nav>
